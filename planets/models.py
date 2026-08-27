b39b50c10529e6cf09157a8e90abb6b04bc05596
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-08-27 11:24:22 +0800

    add owner and plant_class on Planets model

--- planets/models.py
+++ planets/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models import ForeignKey
+
+from accounts.models import Accounts
+from planet_classes.models import PlanetClass
 
 
 class Planets(models.Model):
@@ -7,6 +11,20 @@ class Planets(models.Model):
 	galaxy = models.CharField(max_length=200, null=True, blank=True)
 	star_system = models.CharField(max_length=200, null=True, blank=True)
 	description = models.CharField(max_length=200, null=True, blank=True)
-	discover_date = models.CharField(max_length=200, null=True, blank=True)
+	discover_date = models.DateField(max_length=200, null=True, blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+
+	owner = ForeignKey(
+		Accounts,
+		on_delete=models.SET_NULL,
+		null=True,
+		blank=True
+	)
+
+	planet_class = models.ForeignKey(
+		PlanetClass,
+		on_delete=models.SET_NULL,
+		null=True,
+		blank=True
+	)
